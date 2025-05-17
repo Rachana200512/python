1034c9eb520808f61a75a61b2d0e47c20feb83d9
Rachana_Bhandari05 <rachub745@gmail.com>
2025-05-17 20:58:59 +0545

    Add files via upload
    updated version

--- riddle.py
+++ riddle.py
@@ -5,13 +5,35 @@ questions = ("What is always infront of you but cannot be seen?",
     "What goes up but never comes down?" ,
     "Where does today come  before yesterday?" ,
     "What has words,but never speaks?",
-    "What two things can you never eat before breakfast?")
+    "What two things can you never eat before breakfast?",
+    "The more you take, the more you leave behind. What am I?",
+    "I am an odd number. Take away a letter and I become even. What number am I?",
+    "What is at the end of a rainbow?",
+    "What goes through cities and fields, but never moves?",
+    "I have no life, but I can die. What am I?",
+    "What kind of room has no walls, door or windows?",
+    "What can travel all around the world without leaving its corner?",
+    "What starts with a T, ends with a T, and has T in it?",
+    "What begins with an “e” and only contains one letter?",
+    "Which month has 28 days?"
+     )
 
 answers = (  "future",
            "age",
-           "in dictionary",
+           "dictionary",
             "book",
-            "lunch and dinner")
+            "lunch and dinner",
+            "footsteps",
+            "seven",
+            "w",
+            "road",
+            "battery",
+            "mushroom",
+            "stamp",
+            "teapot",
+            "envelope",
+            "every month"
+            )
 
 guess = []
 score = 0
@@ -21,7 +43,7 @@ for question in questions:
     print("*********************************")
     print(question)
 
-    user_guess = input("Enter your answer:")
+    user_guess = input("Enter your answer:").lower() .strip()
     guess.append(user_guess)
     if user_guess == answers[question_number]:
      score +=1
@@ -32,4 +54,4 @@ for question in questions:
      print(f"the correct answer is {answers[question_number]}")
     question_number += 1
 score = ((score / len(questions)) *100)
-print("your final score is ",score)
+print("your final score is ",score %.2f)
